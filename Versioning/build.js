df22718a9d07752eda5aeb77e1d6db110bea28ba
Alex Naiman <alex.naiman.4@gmail.com>
2019-11-21 11:06:38 +0200

    3.2.5

--- Versioning/build.js
+++ Versioning/build.js
@@ -11,22 +11,34 @@ const position = {
   patch: 2,
 };
 
-try {
-  // eslint-disable-next-line handle-callback-err
-  fs.readFile('./package.json', function(err, content) {
-    const type = process.argv[2] || 'patch';
-
-    pack.version = pack.version
-      .split('.')
-      .map((item, k) => Number(item) + (k === position[type] ? 1 : 0))
-      .reduce((a, b) => `${a}.${b}`);
-    fs.writeFile('./package.json', JSON.stringify(pack), function(err) {
-      if (err) {
-        throw err;
-      }
-      console.log('Current build number: ' + pack.version);
+require('child_process').exec('git rev-parse HEAD', function(err, hash) {
+  console.log('Last commit hash on this branch is:', hash);
+  try {
+    // eslint-disable-next-line handle-callback-err
+    fs.readFile('./package.json', function(err, content) {
+      const type = process.argv[2] || 'patch';
+      pack.sha = hash;
+      pack.version = pack.version
+        .split('.')
+        .map((item, k) => Number(item) + (k === position[type] ? 1 : 0))
+        .reduce((a, b) => `${a}.${b}`);
+      fs.writeFile(
+        './package.json',
+        JSON.stringify(pack),
+        function(err) {
+          if (err) {
+            throw err;
+          }
+          console.log('Current build number: ' + pack.version);
+        },
+        () => {
+          require('child_process').exec(
+            `git add . && git commit -m "${pack.version}" && git push origin`,
+          );
+        },
+      );
     });
-  });
-} catch (e) {
-  console.log(e);
-}
+  } catch (e) {
+    console.log(e);
+  }
+});
